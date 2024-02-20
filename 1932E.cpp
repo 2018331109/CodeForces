fd9ea2a8dcbe508899f9e72d46e6b8607352b8e8
2018331109 <ahmadulsh@gmail.com>
2024-02-20 08:12:04 +0600

    Observation+Math

---
+++ 1932E.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define int long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define neg cout << -1 << endl;
+#define pb push_back
+#define ss second
+#define ff first
+const int INF=1e18;
+const int N=2e5+5;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    vector<int>ans;
+    ans.clear();
+    int total=0;
+    for(auto it:s)
+    {
+        total+=(it-'0');
+    }
+    int carry=0;
+    int nn=n-1;
+    while(nn>=0)
+    {
+        int now=total+carry;
+        ans.pb(now%10);
+        carry=now/10;
+
+        total-=(s[nn]-'0');
+        nn--;
+    }
+
+    if(carry>0)
+    {
+        ans.pb(carry);
+    }
+    int i=ans.size()-1;
+
+    while(ans[i]==0)
+    {
+        i--;
+    }
+    while(i>=0)
+    {
+        cout<<ans[i];
+        i--;
+    }
+    cout<<endl;
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test = 1;
+    cin >> test;
+
+    for (int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+
+    return 0;
+}
