2a2c2d23f257d5723756d0c90d896343d0d79708
2018331109 <ahmadulsh@gmail.com>
2024-03-12 19:06:12 +0600

    Think Think Think

---
+++ 388A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define neg cout << -1 << endl;
+#define pb push_back
+#define ss second
+#define ff first
+const int INF = 1e18;
+const int md = 998244353;
+const int mod = 1e9 + 7;
+const int N = 200005;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    sort(a, a+n);
+    int box=1;
+    for(int i=0; i<n; i++)
+    {
+        if(a[i] < i/box)
+        {
+            box++;
+        }
+    }
+
+    cout<<box<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test = 1;
+    //cin >> test;
+
+    for (int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+
+    return 0;
+}
