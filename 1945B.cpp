702d81317f9a3c0be577421f9cffea33fb8421c6
2018331109 <ahmadulsh@gmail.com>
2024-03-20 20:05:18 +0600

    Thinking

---
+++ 1945B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define neg cout << -1 << endl;
+#define pb push_back
+#define ss second
+#define ff first
+const int INF = 1e18;
+const int md = 998244353;
+const int mod = 1e9 + 7;
+const int N = 200005;
+vector<int>a(N, 0), b(N, 0);
+int n, m, ans=INF;
+
+void solve()
+{
+    int a, b, c;
+    cin>>a>>b>>c;
+    cout<<c/a+1+c/b+1<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test = 1;
+    cin >> test;
+
+    for (int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+
+    return 0;
+}
