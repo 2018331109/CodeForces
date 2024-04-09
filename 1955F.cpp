5fe487a9363f1d2429ae31d9a447ff3b025ded26
2018331109 <ahmadulsh@gmail.com>
2024-04-09 11:13:57 +0600

    There is always a simplest solution to reach. The only thing required is, you have to think think and think

---
+++ 1955F.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define neg cout << -1 << endl;
+#define pb push_back
+#define ss second
+#define ff first
+const int INF = 1e18;
+const int md = 998244353;
+const int mod = 1e9 + 7;
+const int N=2e5+5;
+
+void solve()
+{
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    int ans=d/2;
+    ans+=(a/2)+(b/2)+(c/2);
+    ans+=min(a%2, min(b%2, c%2));
+    cout<<ans<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test = 1;
+    cin >> test;
+
+    for (int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+
+    return 0;
+}
