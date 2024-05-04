ab1213354d63cc8aa70d3a2fb29973574525a683
2018331109 <ahmadulsh@gmail.com>
2024-05-04 06:28:36 +0600

    Graphical Observation

---
+++ 1968E.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define neg cout << -1 << endl;
+#define pb push_back
+#define ss second
+#define ff first
+
+const int md=998244353;
+const int N=200005;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<"1 1"<<endl;
+    cout<<"2 1"<<endl;
+    for(int i=3;i<=n;i++)
+    {
+        cout<<i<<" "<<i<<endl;
+    }
+    cout<<endl;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int test = 1;
+    cin >> test;
+
+    for (int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+
+    return 0;
+}
